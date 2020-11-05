b295201e38176da2a038a850efa03615c2be0be3
Thomas West <thom8750@stud.kea.dk>
2020-11-05 14:18:09 +0100

    Repository update

--- src/main/java/com/datingapp/datingapp/Controller/Controller.java
+++ src/main/java/com/datingapp/datingapp/Controller/Controller.java
@@ -1,15 +1,13 @@
 package com.datingapp.datingapp.Controller;
-
 import com.datingapp.datingapp.Model.CreateProfile;
 import com.datingapp.datingapp.Repositories.AccountRepositories;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import org.springframework.web.bind.annotation.GetMapping;
-
 import java.util.List;
+
 
 public class Controller {
     AccountRepositories accounts = new AccountRepositories();
@@ -21,6 +19,8 @@ public class Controller {
 
         //Data til viewet
         model.addAttribute("allAccounts",allAccounts);
+
+        System.out.println(allAccounts);
 
         return "index";
     }
